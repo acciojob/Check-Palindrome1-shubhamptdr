b2188a464798224a6b08653de79f31ef253b695f
Shubham Patidar <117558233+shubhamptdr@users.noreply.github.com>
2023-04-15 09:05:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // complete the given function
 
 function palindrome(str){
-
+	const tmp = str.slice();
+  console.log(tmp);
+  const resverse = str.split("").reverse().join("");
+  return tmp === resverse;
 }
 module.exports = palindrome
